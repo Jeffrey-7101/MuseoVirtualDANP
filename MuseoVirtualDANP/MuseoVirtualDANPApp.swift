95f9b69cde381094a117cfe7e39c34f4ef89625a
epismac <epismac@epismacs-Mac.local>
2024-10-03 14:57:33 -0500

    first commit

---
+++ MuseoVirtualDANP/MuseoVirtualDANPApp.swift
@@ -0,0 +1,20 @@
+//
+//  MuseoVirtualDANPApp.swift
+//  MuseoVirtualDANP
+//
+//  Created by epismac on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseoVirtualDANPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
