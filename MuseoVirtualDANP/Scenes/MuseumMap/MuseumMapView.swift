46f50d8479b8787f5547607a7bf08d61bb9659d3
Ernesto <esuarezlo@unsa.edu.pe>
2024-10-09 14:58:40 -0500

    add room and expositions color

--- MuseoVirtualDANP/Scenes/MuseumMap/MuseumMapView.swift
+++ MuseoVirtualDANP/Scenes/MuseumMap/MuseumMapView.swift
@@ -1,11 +1,43 @@
 import SwiftUI
 import CoreData
 
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
 // MARK: - Estructura del Modelo
 struct MuseumRoom {
     let id: UUID
     let name: String
     let frame: CGRect
+    let bgColor: String
+    let borderColor: String
+    let drawBorder: Bool
+    
     var expositions: [Exposition]
 }
 
@@ -13,7 +45,15 @@ struct Exposition {
     let id: UUID
     let name: String
     let shape: ShapeType
+    
+    let bgColor: String
+    let borderColor: String
+    let drawBorder: Bool
+    
     let relativeFrame: CGRect
+    
+    
+    
     
     enum ShapeType {
         case rectangle
@@ -24,11 +64,12 @@ struct ExpositionView: View {
     let exposition: Exposition
     let roomOrigin: CGPoint
     let scale: CGFloat
+    let bgColor: String
     let onTap: () -> Void
 
     var body: some View {
         Rectangle()
-            .fill(Color.blue)
+            .fill(Color(hex: bgColor))
             .frame(width: exposition.relativeFrame.width * scale, height: exposition.relativeFrame.height * scale)
             .position(x: (roomOrigin.x + exposition.relativeFrame.midX) * scale, y: (roomOrigin.y + exposition.relativeFrame.midY) * scale)
             .onTapGesture {
@@ -50,8 +91,8 @@ struct RoomView: View {
         ZStack {
             // Sala
             Rectangle()
-                .strokeBorder(Color.black, lineWidth: 3)
-                .background(Rectangle().fill(Color.gray.opacity(0.9)))
+                .strokeBorder(Color(hex: room.borderColor), lineWidth: room.drawBorder ? 3 : 0)
+                .background(Rectangle().fill(Color(hex: room.bgColor).opacity(0.9)))
                 .frame(width: room.frame.width * scale, height: room.frame.height * scale)
                 .position(x: (adjustedOrigin.x + room.frame.width / 2) * scale, y: (adjustedOrigin.y + room.frame.height / 2) * scale)
                 .onTapGesture {
@@ -68,7 +109,7 @@ struct RoomView: View {
 
             // Exposiciones dentro de la sala
             ForEach(room.expositions, id: \.id) { exposition in
-                ExpositionView(exposition: exposition, roomOrigin: adjustedOrigin, scale: scale) {
+                ExpositionView(exposition: exposition, roomOrigin: adjustedOrigin, scale: scale, bgColor: exposition.bgColor) {
                     onTapExposition(exposition)
                 }
             }
@@ -106,11 +147,17 @@ struct MuseumMapView: View {
                     id: room.id!,
                     name: room.name ?? "va",
                     frame: CGRect(x: room.posX, y: room.posY, width: room.width, height: room.height),
+                    bgColor: room.bg_color ?? "#008800",
+                    borderColor: room.border_color ?? "#008850",
+                    drawBorder: room.border,
                     expositions: expositions.map { exposition in
                         Exposition(
                             id: exposition.id!,
                             name: exposition.name ?? "vaa",
                             shape: .rectangle, // Por defecto todas las exposiciones serán rectángulos
+                            bgColor: exposition.bg_color ?? "#008800",
+                            borderColor: exposition.border_color ?? "#508800",
+                            drawBorder: exposition.border,
                             relativeFrame: CGRect(x: exposition.posX, y: exposition.posY, width: exposition.width, height: exposition.height)
                         )
                     }
@@ -178,41 +225,92 @@ func fetchMuseumData() {
 
 func saveRoomsToCoreData(_ roomsData: [MuseumRoomData]) {
     let context = PersistenceController.shared.container.viewContext
-    
-    for roomData in roomsData {
-        if roomData.id == nil {
-            continue
-        }
 
-        // Buscar si ya existe un objeto con el mismo integer_id
-        let fetchRequest: NSFetchRequest<MuseumRoomEntity> = MuseumRoomEntity.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "integer_id == %d", roomData.id!)
-        
+    for roomData in roomsData {
+        // Buscar si ya existe un objeto MuseumRoomEntity con el mismo integer_id
+        let fetchRoomRequest: NSFetchRequest<MuseumRoomEntity> = MuseumRoomEntity.fetchRequest()
+        fetchRoomRequest.predicate = NSPredicate(format: "integer_id == %d", roomData.id!)
+
         do {
-            let fetchedRooms = try context.fetch(fetchRequest)
-            
-            // Si ya existe una sala con el mismo integer_id, la actualizamos
+            let fetchedRooms = try context.fetch(fetchRoomRequest)
+            let roomEntity: MuseumRoomEntity
+
+            // Si ya existe una sala con el mismo integer_id, actualizamos
             if let existingRoom = fetchedRooms.first {
-                existingRoom.name = roomData.nombre
-                existingRoom.posX = roomData.posX
-                existingRoom.posY = roomData.posY
-                existingRoom.width = roomData.width
-                existingRoom.height = roomData.height
+                roomEntity = existingRoom
+                roomEntity.name = roomData.nombre
+                roomEntity.posX = roomData.posX
+                roomEntity.posY = roomData.posY
+                roomEntity.width = roomData.width
+                roomEntity.height = roomData.height
+                
+                roomEntity.bg_color = roomData.bg_color
+                roomEntity.border_color = roomData.border_color
+                roomEntity.border = roomData.border
                 print("Updating existing room with integer_id \(roomData.id!)")
             } else {
                 // Si no existe, creamos una nueva entidad
-                let newRoomEntity = MuseumRoomEntity(context: context)
-                newRoomEntity.id = UUID(uuidString: String(roomData.id!)) ?? UUID()
-                newRoomEntity.integer_id = roomData.id!
-                newRoomEntity.name = roomData.nombre
-                newRoomEntity.posX = roomData.posX
-                newRoomEntity.posY = roomData.posY
-                newRoomEntity.width = roomData.width
-                newRoomEntity.height = roomData.height
+                roomEntity = MuseumRoomEntity(context: context)
+                roomEntity.id = UUID(uuidString: String(roomData.id!)) ?? UUID()
+                roomEntity.integer_id = roomData.id!
+                roomEntity.name = roomData.nombre
+                roomEntity.posX = roomData.posX
+                roomEntity.posY = roomData.posY
+                roomEntity.width = roomData.width
+                roomEntity.height = roomData.height
+                
+                
+                roomEntity.bg_color = roomData.bg_color
+                roomEntity.border_color = roomData.border_color
+                roomEntity.border = roomData.border
                 print("Creating new room with integer_id \(roomData.id!)")
             }
-            
-            // Guarda los cambios en el contexto
+
+            // Iterar sobre las exposiciones de la sala
+            for expositionData in roomData.exposiciones {
+                // Buscar si ya existe un objeto ExpositionEntity con el mismo integer_id
+                let fetchExpositionRequest: NSFetchRequest<ExpositionEntity> = ExpositionEntity.fetchRequest()
+                fetchExpositionRequest.predicate = NSPredicate(format: "integer_id == %d", expositionData.id!)
+
+                let fetchedExpositions = try context.fetch(fetchExpositionRequest)
+                let expositionEntity: ExpositionEntity
+
+                // Si ya existe una exposición con el mismo integer_id, actualizamos
+                if let existingExposition = fetchedExpositions.first {
+                    expositionEntity = existingExposition
+                    expositionEntity.name = expositionData.titulo
+                    expositionEntity.posX = expositionData.posX
+                    expositionEntity.posY = expositionData.posY
+                    expositionEntity.width = expositionData.width
+                    expositionEntity.height = expositionData.height
+                    
+                    
+                    expositionEntity.bg_color = expositionData.bg_color
+                    expositionEntity.border_color = expositionData.border_color
+                    expositionEntity.border = expositionData.border
+                    print("Updating existing exposition with integer_id \(expositionData.id!)")
+                } else {
+                    // Si no existe, creamos una nueva entidad
+                    expositionEntity = ExpositionEntity(context: context)
+                    expositionEntity.id = UUID(uuidString: String(expositionData.id!)) ?? UUID()
+                    expositionEntity.integer_id = expositionData.id!
+                    expositionEntity.name = expositionData.titulo
+                    expositionEntity.posX = expositionData.posX
+                    expositionEntity.posY = expositionData.posY
+                    expositionEntity.width = expositionData.width
+                    expositionEntity.height = expositionData.height
+                    
+                    expositionEntity.bg_color = expositionData.bg_color
+                    expositionEntity.border_color = expositionData.border_color
+                    expositionEntity.border = expositionData.border
+                    print("Creating new exposition with integer_id \(expositionData.id!)")
+                }
+
+                // Agregar la relación entre la sala y la exposición
+                roomEntity.addToExpositions(expositionEntity)
+            }
+
+            // Guardar los cambios en el contexto
             try context.save()
         } catch {
             print("Error al intentar guardar o actualizar la entidad: \(error)")
@@ -229,6 +327,9 @@ struct MuseumRoomData: Codable {
     let width: Double
     let height: Double
     let exposiciones: [ExpositionData]
+    let bg_color: String
+    let border_color: String
+    let border: Bool
 }
 
 struct ExpositionData: Codable {
@@ -238,5 +339,8 @@ struct ExpositionData: Codable {
     let posY: Double
     let width: Double
     let height: Double
+    let bg_color: String
+    let border_color: String
+    let border: Bool
 }
 
